96396d6382365fc3ff2be91810a5d2f5ef8c465e
rinconl <luis.rincon@dowjones.com>
2021-03-09 21:12:19 -0500

    adding a new function

--- system/.function.zsh
+++ system/.function.zsh
@@ -1,6 +1,6 @@
 # Opens github to create a pull request
 function openpr() {
-  # github_url and branch_name are piping the result of each command with the "|" sign. 
+  # github_url and branch_name are piping the result of each command with the "|" sign.
   github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's#.net:#.net/#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
   branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
   pr_url=$github_url"/compare/"$branch_name
@@ -12,7 +12,7 @@ function openpr() {
 function create-pr() {
   git push origin HEAD
 
-  if [ $? -eq 0 ]; then # Checks the exit status of the last command, which is the one above ^^ "git push origin HEAD". If the status is equal(-eq) to 0, it means success. 
+  if [ $? -eq 0 ]; then # Checks the exit status of the last command, which is the one above ^^ "git push origin HEAD". If the status is equal(-eq) to 0, it means success.
     openpr
   else
     echo 'failed to push commits and open a pull request.';
@@ -30,7 +30,7 @@ function show-path() {
 
   echo ""
   echo "${GREEN}Home directory or '~/' --> ${NOCOLOR} $HOME"
-  
+
   read -rA arrOfFiles <<< "$str" # str is read into an array (arrOfFiles) as tokens (empty space) separated by IFS. Note: in bash it can be "read -ra" but ZSH for some requires a capital "A".
   echo ""
   echo "${RED}Your executable files in PATH:${NOCOLOR}"
@@ -43,4 +43,9 @@ function show-path() {
   echo ""
   echo "${GREEN}Original PATH string${NOCOLOR} --> $PATH"
   IFS=""
+}
+
+function open-project() {
+  jump $1
+  atom $PWD
 }
